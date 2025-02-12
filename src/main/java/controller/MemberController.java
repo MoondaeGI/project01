9ad246061640b9bce62ea63943075d349a2dd182
qhfka74 <shj04367@gmail.com>
2025-02-12 15:44:40 +0900

    controller created3

--- src/main/java/controller/MemberController.java
+++ src/main/java/controller/MemberController.java
@@ -13,7 +13,14 @@ public class MemberController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		String cmd = request.getRequestURI();
+		System.out.println("if바깥 호출");
 
+		if (cmd.equals("/add.member")) {
+			System.out.println("if안의 호출");
+			// request.getRequestDispatcher("/member/test.jsp").forward(request, response);
+			response.sendRedirect("/member/test.jsp");
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
